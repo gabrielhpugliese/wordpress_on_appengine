0e14c386cb4c4c23565ffc219b88e118bfd3d1b3
gabrielhpugliese <gabrielhpugliese@gmail.com>
2013-06-03 01:23:42 -0300

    Adding cache, better install.sh and splitted packages.tar.gz

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-mysql -u root -p < database.sql
+echo "Downloading GAE SDK..."
 wget http://commondatastorage.googleapis.com/appengine-php/appengine-php-sdk-1.8.0.zip
 unzip appengine-php-sdk-1.8.0.zip 
 rm appengine-php-sdk-1.8.0.zip
+
+echo "Creating WP databases..."
+echo "Enter MySQL username:"
+read mysql_user
+mysql -u $mysql_user -p < database.sql
+
 cd project
-tar -xvzf packages.tar.gz
-rm packages.tar.gz
+tar -xvzf php.tar.gz 
+tar -xvzf wordpress.tar.gz
+rm php.tar.gz wordpress.tar.gz
