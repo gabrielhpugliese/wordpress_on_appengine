61ab943817c4e1d823acf3f5d2dd1077f9a9e5a2
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-06-17 17:44:52 -0300

    Adding gae plugin

--- install.sh
+++ install.sh
@@ -2,17 +2,12 @@
 
 echo "Downloading GAE SDK..."
 wget http://commondatastorage.googleapis.com/appengine-php/appengine-php-sdk-1.8.0.zip
-unzip appengine-php-sdk-1.8.0.zip 
+unzip appengine-php-sdk-1.8.0.zip
 rm appengine-php-sdk-1.8.0.zip
 
 echo "Creating WP databases..."
 echo "Enter MySQL username:"
 read mysql_user
 mysql -u $mysql_user -p < database.sql
-
-cd project
-tar -xvzf php.tar.gz 
-tar -xvzf wordpress.tar.gz
-rm php.tar.gz wordpress.tar.gz
 
 echo "WP on GAE installation finished. Now run 'bash run.sh' to start the server."
