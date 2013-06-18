50e62b7683f5b6d208e5d9b148e3b50f4facd169
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-06-18 11:58:28 -0300

    Better deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-echo "This will create a database and username as described in database.sql."
-echo "Enter your instance complete name (eg.: my-project:instance):"
-read instance_name
-google_sql/google_sql.sh $instance_name < database.sql
-echo "Done creating database. Starting to deploy."
+function deploy {
+    echo "Enter your instance complete name (eg.: my-project:instance):"
+    read instance_name
+    google_sql/google_sql.sh $instance_name < database.sql
+    if [[ $? -ne 0 ]] ; then
+        exit 1;
+    fi
+    echo "Done creating database. Let's to deploy."
 
-google_appengine/appcfg.py -R update project/
+    google_appengine/appcfg.py -R update project/
+    exit 0;
+}
+
+
+echo "This will create a database and username as described in database.sql and application name described in project/app.yaml"
+echo "Before continuing, edit those files with strong passwords and proper application name."
+read -r -p "Do you want to continue? [y/n]" response
+response=${response,,}
+case "$response" in
+  y|Y ) deploy
+  ;;
+  * ) exit 0
+  ;;
+esac
+
+
